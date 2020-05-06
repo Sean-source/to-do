a1473958d61a0137be01da88340517e0e573e3c8
Sean Baklini <sbaklini@unm.edu>
2020-05-05 21:18:18 -0600

    Initializing the file structure

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO actionItems (name, description, complete) VALUES ('Complete excel template', 'Conduct data analysis to be presented to management',false);
+
+INSERT INTO actionItems (name, description, complete) VALUES ('Finish programming to-do application', 'Create the front and back-end of the application',false);
+
+INSERT INTO actionItems (name, description, complete) VALUES ('Go to the gym', 'Squats and cable rows',false);
